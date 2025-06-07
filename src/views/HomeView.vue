27803026ed46865043628991acc8bcf02df95246
N00BMASTER1524 <ziyaan1524@gmail.com>
2025-06-08 02:06:15 +0500

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Hero from '@/components/Hero.vue';
+import Herocards from '@/components/Herocards.vue';
+import JobListings from '@/components/JobListings.vue';
+</script>
+
+<template>
+  <Hero/>
+  <Herocards/>
+  <JobListings :limit="3" :showButton="true"/>
+</template>
